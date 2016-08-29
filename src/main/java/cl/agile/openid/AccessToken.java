122e9b26db26d6b526aae3f178f07032744efea5
agilecl <dev@agile.cl>
2016-08-28 22:44:07 -0300

    Update AccessToken.java

--- src/main/java/cl/agile/openid/AccessToken.java
+++ src/main/java/cl/agile/openid/AccessToken.java
@@ -1,5 +1,13 @@
 package cl.agile.openid;
 
+/*
+	 * Ejemplo de implementación ClaveUnica OpenIdConnect
+	 * Author: Claudio Delgado
+	 * Mail: dev@agile.cl
+	 * Agile Ingeniería & Consultoría
+	 * www.agile.cl    
+ */
+
 public class AccessToken {
 	
 	public String access_token;
