8dd9e115a343ea78d25e1f23b08b81facb4b03f5
Claudio Delgado <claudio.delgado@agile.cl>
2016-08-28 21:33:55 -0400

    Se mejora web.xml y se agrega página de resultado

---
+++ src/main/java/cl/agile/openid/ShowResult.java
@@ -0,0 +1,50 @@
+package cl.agile.openid;
+
+import java.io.IOException;
+import java.net.URI;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ShowResult
+ */
+public class ShowResult extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowResult() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		
+		String nombres = request.getParameter("nombres");
+
+		String appellidoPaterno = request.getParameter("appellidoPaterno");
+
+		String appellidoMaterno = request.getParameter("appellidoMaterno");
+
+		String rut = request.getParameter("RUT");
+		
+	   ServletOutputStream out = response.getOutputStream();
+		out.println("Nombres " + nombres );
+		out.println("A. Paterno " + appellidoPaterno );
+		out.println("A. Materno " + appellidoMaterno  );
+		out.println("RUT " + rut  );
+
+	}
+
+}
