b18dbcf98da54dfa1f9d4a855ec706fd4dc19020
agilecl <dev@agile.cl>
2016-12-21 17:00:02 -0300

    Update AccessToken.java
    Se agrega refresh_token

--- src/main/java/cl/agile/openid/AccessToken.java
+++ src/main/java/cl/agile/openid/AccessToken.java
@@ -14,6 +14,14 @@ public class AccessToken {
 	public String token_type;
 	public int expires_in;
 	public String id_token;
+	public String refresh_token;
+	
+	public String getRefresh_token() {
+		return refresh_token;
+	}
+	public void setRefresh_token(String refresh_token) {
+		this.refresh_token = refresh_token;
+	}
 	
 	public String getAccess_token() {
 		return access_token;
